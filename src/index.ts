a0108f9c2e97e39e56c0f2905ad87eaf137f5939
Bekter <besedov347@gmail.com>
2023-12-07 16:48:13 +0300

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,16 @@
+const star = (): string => {
+    let result = ''
+    for (let i: number = 5; i >= 0; i--) {
+        for (let j: number = 0; j <= i; j++) {
+            result += " "
 
-const findRepeatingNumbers_1 = (array: number[]): number[] => {
-    let result: number[] = [];
-    for (let i: number = 0; i < array.length; i++) {
-        for (let j: number = 0; j < array.length; j++)
-            if ((i !== j) && (array[j] == array[i])) {
-                result.push(array[i]);
-            }
+        }
+        for (let k: number = 0; k < 1; k++) {
+            result += "*"
+        }
+        result += "\n"
     }
-    return [...new Set([...result])]
+    return result
 }
 
-const findRepeatingNumbers_2 = (array: number[]) => [...new Set([...array.filter((e, f) => array.find(i => f != i && e == array[i]))])]
-
-// const findLargestNumber_2 = (array: number[]): number => array.sort((a, b) => a - b)[array.length - 1]
-
-// const findLargestNumber_3 = (array: number[]): number => Math.max(...array)
-
-console.log(findRepeatingNumbers_2([8, 1, 2, 3, 8, 4, 5, 1, 8, 3]));
+console.log(star());
