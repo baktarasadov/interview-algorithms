727d86d87a0c6959edcbb48d76f6d9cf9f3283fc
Bekter <besedov347@gmail.com>
2023-12-07 12:58:28 +0300

    factorial added

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// interface IResponse {
+//     result: number;
+//     array: number[];
+// }
+
+// const factorial = (number: number): IResponse => {
+//     let result: number = 1;
+//     let array: number[] = [];
+//     for (let i: number = 1; i <= number; i++) {
+//         result *= i
+//         array.push(result);
+//     }
+//     return { result, array };
+
+// }
+
+// console.log(factorial(5));
